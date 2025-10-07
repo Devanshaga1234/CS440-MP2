5ff09b9ee4ef15c59682f03ff9588addc5353e99
da30 <da30@illinois.edu>
2025-10-07 17:01:25 -0500

    Fix errors

--- mp2/src/api/financialDataApi.ts
+++ mp2/src/api/financialDataApi.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = '/fd';
-const API_KEY = process.env.REACT_APP_FINANCIALDATA_API_KEY || '11bae4eeb55a588482a49856b5fa63c6';
+const API_KEY = process.env.REACT_APP_FINANCIALDATA_API_KEY || '69d88fba77fd52259be201d482e4cccf';
 
 const api = axios.create({ baseURL: BASE_URL });
 
